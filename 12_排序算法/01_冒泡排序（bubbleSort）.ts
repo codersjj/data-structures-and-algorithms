4f1f4260b0031eb4a3028ca1e26142ac835188f7
codersjj <shanewestlife@outlook.com>
2023-09-28 16:21:25 +0800

    chore(冒泡排序): 移除注释

--- 12_排序算法/01_冒泡排序（bubbleSort）.ts
+++ 12_排序算法/01_冒泡排序（bubbleSort）.ts
@@ -6,18 +6,9 @@ import {
 } from "./utils"
 
 function bubbleSort(arr: number[]): number[] {
-  // n 刚好表示数据量，后续可以用来在大 O 表示法中进行算法复杂度分析
   const n = arr.length
-  // let count = 0
-  // 外层 for 循环：0~n-2
   for (let i = 0; i < n - 1; i++) {
-    // console.log(`开始第${++count}轮冒泡...`)
     let swapped = false
-    // i = 0     => j < n - 1
-    // i = 1     => j < n - 1 - 1
-    // ...
-    // i = n - 2 => j < 1
-    // 内层 for 循环：找到（本轮循环中的）最大值
     for (let j = 0; j < n - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1)
@@ -25,7 +16,6 @@ function bubbleSort(arr: number[]): number[] {
       }
     }
     if (!swapped) break
-    // console.log(`第${count}轮冒泡结束`)
   }
 
   return arr
