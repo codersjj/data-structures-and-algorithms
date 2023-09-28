da7a568e0736d16cc89b4106b09ac8f0455920a3
codersjj <shanewestlife@outlook.com>
2023-09-28 16:19:56 +0800

    perf(冒泡排序): 添加 swapped 判断，提前退出外层循环 如果在某一轮冒泡中没有发生元素交换，那么数组已经有序，可以提前退出循环，而不必继续执行多余的比较操作。

--- 12_排序算法/01_冒泡排序（bubbleSort）.ts
+++ 12_排序算法/01_冒泡排序（bubbleSort）.ts
@@ -8,8 +8,11 @@ import {
 function bubbleSort(arr: number[]): number[] {
   // n 刚好表示数据量，后续可以用来在大 O 表示法中进行算法复杂度分析
   const n = arr.length
+  // let count = 0
   // 外层 for 循环：0~n-2
   for (let i = 0; i < n - 1; i++) {
+    // console.log(`开始第${++count}轮冒泡...`)
+    let swapped = false
     // i = 0     => j < n - 1
     // i = 1     => j < n - 1 - 1
     // ...
@@ -18,8 +21,11 @@ function bubbleSort(arr: number[]): number[] {
     for (let j = 0; j < n - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1)
+        swapped = true
       }
     }
+    if (!swapped) break
+    // console.log(`第${count}轮冒泡结束`)
   }
 
   return arr
