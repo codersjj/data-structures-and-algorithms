a07eaa8ee0ee37a85a0a3fd45c826fc54f08b0ca
codersjj <shanewestlife@outlook.com>
2023-09-28 18:08:02 +0800

    feat(选择排序): 第二轮选择的实现 实现将第二小的值排到第二位

--- 12_排序算法/02_选择排序（selectionSort）.ts
+++ 12_排序算法/02_选择排序（selectionSort）.ts
@@ -14,6 +14,17 @@ function selectionSort(arr: number[]): number[] {
   // 每一轮找到最小值后，只进行一次交换即可
   swap(arr, 0, minIndex)
 
+  // 第二轮
+  minIndex = 1
+  // 从索引 2 开始找
+  for (let j = 2; j < n; j++) {
+    if (arr[j] < arr[minIndex]) {
+      minIndex = j
+    }
+  }
+  // 每一轮找到最小值后，只进行一次交换即可
+  swap(arr, 1, minIndex)
+
   return arr
 }
 
