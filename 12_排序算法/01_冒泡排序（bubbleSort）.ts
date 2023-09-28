4b29957592e2ce97115e2ca790f539d58ec8de15
codersjj <shanewestlife@outlook.com>
2023-09-28 15:49:34 +0800

    perf(冒泡排序): 只需要进行 n - 1 轮冒泡

--- 12_排序算法/01_冒泡排序（bubbleSort）.ts
+++ 12_排序算法/01_冒泡排序（bubbleSort）.ts
@@ -8,12 +8,12 @@ import {
 function bubbleSort(arr: number[]): number[] {
   // n 刚好表示数据量，后续可以用来在大 O 表示法中进行算法复杂度分析
   const n = arr.length
-  // 外层 for 循环：0~n-1
-  for (let i = 0; i < n; i++) {
+  // 外层 for 循环：0~n-2
+  for (let i = 0; i < n - 1; i++) {
     // i = 0     => j < n - 1
     // i = 1     => j < n - 1 - 1
     // ...
-    // i = n - 1 => j < 0
+    // i = n - 2 => j < 1
     // 内层 for 循环：找到（本轮循环中的）最大值
     for (let j = 0; j < n - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
