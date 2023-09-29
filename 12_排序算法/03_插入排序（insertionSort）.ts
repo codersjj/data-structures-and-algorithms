45ee7184bf9e5c78b76c5492d036a34bf70264c5
codersjj <shanewestlife@outlook.com>
2023-09-29 18:07:54 +0800

    feat(插入排序): 第二轮插入的实现 实现前三个数有序

--- 12_排序算法/03_插入排序（insertionSort）.ts
+++ 12_排序算法/03_插入排序（insertionSort）.ts
@@ -4,7 +4,7 @@ function insertionSort(arr: number[]): number[] {
 
   // 8 6 3 20 2 => 6 8 3 20 2
   // 第一轮：实现前两个数有序
-  const currNum = arr[1]
+  let currNum = arr[1]
   let j = 0
   // 存在前面的数比当前的数大时，进入循环
   while (arr[j] > currNum && j >= 0) {
@@ -14,6 +14,16 @@ function insertionSort(arr: number[]): number[] {
     j--
   }
   // 将当前的数插入到已找到的比其小（或相等）的数的后一个位置，或者 j 已经等于 -1 时，插入到第一个位置（索引为 0）
+  arr[j + 1] = currNum
+
+  // 8 6 3 20 2 => 6 8 3 20 2 => 3 6 8 20 2
+  // 第二轮：实现前三个数有序
+  currNum = arr[2]
+  j = 1
+  while (arr[j] > currNum && j >= 0) {
+    arr[j + 1] = arr[j]
+    j--
+  }
   arr[j + 1] = currNum
 
   return arr
