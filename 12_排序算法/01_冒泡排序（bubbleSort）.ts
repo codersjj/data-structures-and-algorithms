5f53d7f0b691205b088640e1148293658065a633
codersjj <shanewestlife@outlook.com>
2023-09-29 15:54:58 +0800

    chore(冒泡排序): 移除注释

--- 12_排序算法/01_冒泡排序（bubbleSort）.ts
+++ 12_排序算法/01_冒泡排序（bubbleSort）.ts
@@ -10,27 +10,18 @@ function bubbleSort(arr: number[]): number[] {
   const n = arr.length
   let lastIndex = n - 1
   let lastSwappedIndex = n - 1
-  // let round = 0
-  // let compareCount = 0
-  // let swapCount = 0
   for (let i = 0; i < n - 1; i++) {
-    // round++
     let swapped = false
     for (let j = 0; j < lastIndex; j++) {
-      // compareCount++
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1)
         swapped = true
         lastSwappedIndex = j
-        // swapCount++
       }
     }
     if (!swapped) break
     lastIndex = lastSwappedIndex
   }
-
-  // console.log(`总共进行了 ${round} 轮冒泡`)
-  // console.log(`总共比较了 ${compareCount} 次，交换了 ${swapCount} 次`)
   return arr
 }
 
