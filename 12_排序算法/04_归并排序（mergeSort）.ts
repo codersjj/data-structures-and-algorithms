964a06f79139c349b485b72715d65bc15287f42f
codersjj <shanewestlife@outlook.com>
2023-10-05 18:05:53 +0800

    feat(归并排序): 原地排序 更多内容：https://www.geeksforgeeks.org/in-place-merge-sort/

--- 12_排序算法/04_归并排序（mergeSort）.ts
+++ 12_排序算法/04_归并排序（mergeSort）.ts
@@ -44,5 +44,59 @@ function mergeSort(arr: number[]): number[] {
   return newArr
 }
 
+function mergeSortInPlace(arr: number[]): number[] {
+  const n = arr.length
+
+  // 归并排序的内部实现函数，对数组 arr 的 [left, right] 区间进行排序
+  function mergeSort(arr: number[], left: number, right: number): void {
+    // 如果区间只有一个元素或者为空，直接返回
+    if (left >= right) return
+
+    // 1. divide
+    // 与 (left + right) / 2 相同，但避免了对大型 left 和 right 的溢出问题。
+    const mid = left + Math.floor((right - left) / 2)
+    // 递归，对左右两个子区间进行排序
+    mergeSort(arr, left, mid)
+    mergeSort(arr, mid + 1, right)
+
+    // 2. merge
+    // 用一个临时数组存放合并结果
+    const temp: number[] = []
+    // 双指针
+    let i = left
+    let j = mid + 1
+    // 比较左右两个子区间的元素，将较小的元素插入到 temp 中
+    while (i <= mid && j <= right) {
+      if (arr[i] <= arr[j]) {
+        temp.push(arr[i])
+        i++
+      } else {
+        temp.push(arr[j])
+        j++
+      }
+    }
+    // 如果左子区间还有元素，将它们全部复制到 temp 中
+    while (i <= mid) {
+      temp.push(arr[i])
+      i++
+    }
+    // 如果右子区间还有元素，将它们全部复制到 temp 中
+    while (j <= right) {
+      temp.push(arr[j])
+      j++
+    }
+    // 将 temp 中的元素复制回原数组中
+    for (let i = 0; i < temp.length; i++) {
+      arr[left + i] = temp[i]
+    }
+  }
+
+  mergeSort(arr, 0, n - 1)
+
+  return arr
+}
+
 testSort(mergeSort)
 measureSort(mergeSort)
+testSort(mergeSortInPlace)
+measureSort(mergeSortInPlace)
