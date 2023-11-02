52c6de3a63bda4d0134ee1b6840a31b3163ba5aa
codersjj <shanewestlife@outlook.com>
2023-11-02 10:19:24 +0800

    chore: practice-bubble sort

---
+++ 12_排序算法/practice/20231102/bubbleSort.ts
@@ -0,0 +1,16 @@
+function bubbleSort(arr: number[]): number[] {
+  const n = arr.length
+
+  for (let i = 0; i < n - 1; i++) {
+    let swapped = false
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        swapped = true
+      }
+    }
+    if (!swapped) break
+  }
+
+  return arr
+}
